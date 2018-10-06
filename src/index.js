1bfad30eccf931ad31f59eeeaae8adc4f365dd7d
olexxxey <alexeykorneyevets@gmail.com>
2018-10-06 19:08:18 +0300

    getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let primes = [];
+    let arr = [];
+    let zeros = [];
+    let sum = 0;
+
+    for (let i = 2; i <= base; i++) {
+        let meter = 0;
+        if (base % i == 0) primes.push(i);
+        while (base % i == 0) {
+            base /= i;
+            meter++;
+        }
+        if (meter > 0)  arr.push(meter);
+    }
+
+    primes.forEach((prime, index) => {
+        let deg = 2;
+        while (number / prime > 1) {
+            sum += Math.floor(number / prime);
+            prime = Math.pow(primes[index], deg);
+            deg++;
+        }
+        zeros.push(sum);
+        sum = 0;
+    })
+
+    return zeros.map((zero, index) => Math.floor(zero/arr[index])).sort((a, b) => a-b)[0];
 }
